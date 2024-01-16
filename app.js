49a0d6ed866830822c4d6c7b451a02548a3648c8
IamHemant001 <146627413+IamHemant001@users.noreply.github.com>
2024-01-16 20:52:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 let gameSeq = [];
 let userSeq = [];
 let Hscore = 0;
+let gOh1 = document.querySelector("#game-over");
 let h2 = document.querySelector("h2");
 let startBtn = document.getElementById("start-btn");
 let btns = ["green","red","yellow","voilet"];
 
+const audio1 = new Audio();
+audio1.src = "start-13691.mp3";
+const audio2 = new Audio();
+audio2.src = "stop-13692.mp3";
+const audio3 = new Audio();
+audio3.src = "short-beep-tone-47916.mp3";
+const audio4 = new Audio();
+audio4.src = "achive-sound-132273.mp3";
+
 let started = false;
 let level = 0;
+
 
 startBtn.addEventListener("click",function(event){
     if(started == false){
@@ -16,8 +27,12 @@ startBtn.addEventListener("click",function(event){
     }
 })
 
-function gameFlash(btn){
+function gameFlash(btn,audio){
     btn.classList.add("gameflash");
+
+    setTimeout(function(){
+        audio.play();
+    },50)
 
     setTimeout(function(){
         btn.classList.remove("gameflash")
@@ -25,9 +40,13 @@ function gameFlash(btn){
 }
 
 
-function userFlash(btn){
+function userFlash(btn,audio){
     btn.classList.add("userflash");
-    
+
+    setTimeout(function(){
+        audio.play();
+    },10)
+
     setTimeout(function(){
         btn.classList.remove("userflash")
     },300);
@@ -42,10 +61,22 @@ function levelUp(){
     let randIdx = Math.floor(Math.random() *3);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
+
+    let audio; 
+
+    if(randIdx == 0){
+        audio = audio1;
+    }else if (randIdx == 1){
+        audio = audio2;
+    }else if (randIdx ==2){
+        audio = audio3;
+    }else if (randIdx == 3){
+        audio = audio4;
+    }
     gameSeq.push(randColor);
     console.log(gameSeq)
 
-    gameFlash(randBtn);
+    gameFlash(randBtn,audio);
 }
 
 function checkAns(idx){
@@ -56,7 +87,9 @@ function checkAns(idx){
             setTimeout(levelUp,1000);
         }
     }else{
-        h2.innerHTML = `Your <b>score<b> was <b> ${level}<b><br>&nbsp;&nbsp;&nbsp;&nbsp;<strong>Game Over!</strong>`;
+        gOh1.style.display = "block";
+        gOh1.innerText = "GAME OVER!";
+        h2.innerHTML = `Your Score ${level}`;
         highScore(level);
         reset();
     }
@@ -64,7 +97,17 @@ function checkAns(idx){
 
 function btnPress(){
     let btn = this;
-    userFlash(btn);
+    let audio;
+    if(btn.id == "green"){
+        audio = audio1;
+    }else if(btn.id == "red"){
+        audio = audio2;
+    }else if(btn.id == "yellow"){
+        audio = audio3;
+    }else if(btn.id == "voilet"){
+        audio = audio4;
+    }
+    userFlash(btn,audio);
 
     let userColor = btn.getAttribute("id");
     userSeq.push(userColor)
@@ -88,16 +131,21 @@ function highScore(level){
 }
 
 function reset(){
-    startBtn.innerText = "Play Again";
+    startBtn.innerText = "Play Again"
     startBtn.style.fontSize = "1.5rem";
-    startBtn.style.display = "inline";
-    document.querySelector("body").style.backgroundColor = "rgb(239, 19, 19)";
+    startBtn.style.display = "block";
+    startBtn.style.height = "4rem";
+    document.querySelector("body").style.backgroundColor = "red";
+    setTimeout(function fun(){
+        document.querySelector("body").style.backgroundColor = "black";
+    }, 150);
+
     userSeq = [];
     gameSeq = [];
     level = 0;
     started = false;
     startBtn.addEventListener("click",function(event){
-        document.querySelector("body").style.backgroundColor = "rgba(249, 78, 197, 0.4)";
+        gOh1.style.display = "none";
         console.log("game stated");
         if(started == false){
         started = true;
@@ -105,3 +153,4 @@ function reset(){
     }
     })
 }
+
